5f2d97682fbd63024c5ab53f791053a8c128bf07
AnnKasian <annkasian.and@gmail.com>
2025-06-18 13:16:28 +0300

    * fix weather repository integration tests

--- tests/weather/mock-data/weather-service.mock.ts
+++ tests/weather/mock-data/weather-service.mock.ts
@@ -1,5 +1,9 @@
 const WeatherMock = {
-  request: { corectCity: "Lviv", wrongCity: "12" },
+  request: {
+    key: "test-api-key",
+    corectCity: "Lviv",
+    wrongCity: "12",
+  },
   response: { temperature: 20, humidity: 50, description: "Sunny" },
   apiResponse: {
     current: { temp_c: 20, condition: { text: "Sunny" }, humidity: 50 },
--- tests/weather/weather.repository.integration.spec.ts
+++ tests/weather/weather.repository.integration.spec.ts
@@ -12,6 +12,11 @@ describe("WeatherRepository  Integration Tests", () => {
   let server: nock.Scope;
 
   beforeAll(async () => {
+    const TEST_API_URL = "https://api.weatherapi/current";
+    const TEST_API_KEY = "test-api-key";
+
+    process.env.API_URL = TEST_API_URL;
+    process.env.API_KEY = TEST_API_KEY;
     module = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({
@@ -26,7 +31,7 @@ describe("WeatherRepository  Integration Tests", () => {
   });
 
   beforeEach(() => {
-    server = nock("https://api.weatherapi.com");
+    server = nock("https://api.weatherapi");
   });
 
   afterEach(() => {
@@ -34,18 +39,26 @@ describe("WeatherRepository  Integration Tests", () => {
   });
 
   afterAll(async () => {
+    nock.restore();
     await module.close();
+    delete process.env.API_URL;
+    delete process.env.API_KEY;
   });
 
   describe("get", () => {
     let handler: nock.Interceptor;
 
     beforeEach(() => {
-      handler = server.get("/v1/current.json");
+      handler = server.get("/current");
     });
 
     it("should get weather data for city", async () => {
-      handler.query(true).reply(200, WeatherMock.apiResponse);
+      handler
+        .query({
+          key: WeatherMock.request.key,
+          q: WeatherMock.request.corectCity,
+        })
+        .reply(200, WeatherMock.apiResponse);
 
       const result = await repository.get(WeatherMock.request.corectCity);
       expect(result).toEqual(WeatherMock.response);
@@ -53,7 +66,10 @@ describe("WeatherRepository  Integration Tests", () => {
 
     it("should return null when city not found", async () => {
       handler
-        .query(true)
+        .query({
+          key: WeatherMock.request.key,
+          q: WeatherMock.request.wrongCity,
+        })
         .reply(400, { error: { code: 1006, message: expect.any(String) } });
 
       const result = await repository.get(WeatherMock.request.wrongCity);
@@ -62,7 +78,10 @@ describe("WeatherRepository  Integration Tests", () => {
 
     it("should throw HttpException if serwer is not responding", async () => {
       handler
-        .query(true)
+        .query({
+          key: WeatherMock.request.key,
+          q: WeatherMock.request.corectCity,
+        })
         .reply(500, { error: { code: 500, message: expect.any(String) } });
 
       await expect(
