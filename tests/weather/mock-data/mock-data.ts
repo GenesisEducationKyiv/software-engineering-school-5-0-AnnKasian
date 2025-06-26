bf73ded903962bacecb68c93c7ebaeeb21db2f13
AnnKasian <annkasian.and@gmail.com>
2025-06-26 14:24:44 +0300

    * fix improve consistency

--- tests/weather/mock-data/mock-data.ts
+++ tests/weather/mock-data/mock-data.ts
@@ -1,2 +1,1 @@
 export { WeatherMock } from "./weather-service.mock.js";
-export { WeatherE2eMock } from "./weather.e2e.mock.js";
--- tests/weather/mock-data/weather.e2e.mock.ts
+++
@@ -1,6 +0,0 @@
-const WeatherE2eMock = {
-  validCity: "Kyiv",
-  invalidCity: "5",
-};
-
-export { WeatherE2eMock };
